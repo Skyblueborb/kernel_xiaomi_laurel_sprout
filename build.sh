0788160b7a108c6414672981519b6c47a545971b
aleeeee1 <pace.alessandro05@protonmail.com>
2024-04-04 10:34:46 +0200

    made build.sh a bit fancier

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Define colors
+GREEN="\e[1;32m"
+RED="\e[1;31m"
+YELLOW="\e[1;33m"
+CYAN="\e[1;36m"
+DEFAULT="\e[0m"
+
 # Define variables
 CLANG_VER="clang-r498229b"
 ROM_PATH="/mnt/QuickBoi/LineageOS/21"
@@ -10,11 +17,11 @@ if [ -d "$ROM_PATH/prebuilts/clang/host/linux-x86/$CLANG_VER" ]; then
 elif [ -d "$HOME/toolchains/neutron-clang" ]; then
     CLANG_DIR="$HOME/toolchains/neutron-clang"
 else
-    echo "Could not find the specified clang directory."
+    echo -e "${RED}Could not find the specified clang directory.${DEFAULT}"
     exit 1
 fi
 
-echo "Using clang directory: $CLANG_DIR"
+echo -e "${YELLOW}Using clang directory: $CLANG_DIR${DEFAULT}"
 
 
 KERNEL_DIR=$PWD
@@ -31,18 +38,18 @@ BUILD_START=$(date +"%s")
 export TARGET_KERNEL_CLANG_COMPILE=true
 PATH="$CLANG_DIR/bin:${PATH}"
 
-echo -e "***********************************************"
-echo    "          Compiling Void Kernel                "
-echo -e "***********************************************"
+echo -e "${CYAN}***********************************************${DEFAULT}"
+echo -e "${CYAN}          Compiling Void Kernel                ${DEFAULT}"
+echo -e "${CYAN}***********************************************${DEFAULT}"
 
 # Finally build it
 mkdir -p out
 make O=out ARCH=arm64 vendor/laurel_sprout-perf_defconfig
 make -j$(nproc --all) O=out ARCH=arm64 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=$CLANG_DIR/bin/llvm- LLVM=1 LLVM_IAS=1 Image.gz-dtb dtbo.img || exit
 
-echo -e "***********************************************"
-echo    "                Zipping Kernel                 "
-echo -e "***********************************************"
+echo -e "${CYAN}***********************************************${DEFAULT}"
+echo -e "${CYAN}                Zipping Kernel                 ${DEFAULT}"
+echo -e "${CYAN}***********************************************${DEFAULT}"
 
 # Create the flashable zip
 cp out/arch/arm64/boot/Image.gz-dtb $Anykernel_DIR
@@ -50,9 +57,9 @@ cp out/arch/arm64/boot/dtbo.img $Anykernel_DIR
 cd $Anykernel_DIR
 zip -r9 $FINAL_ZIP.zip * -x .git README.md *placeholder
 
-echo -e "***********************************************"
-echo    "                 Cleaning up                   "
-echo -e "***********************************************"
+echo -e "${CYAN}***********************************************${DEFAULT}"
+echo -e "${CYAN}                 Cleaning up                   ${DEFAULT}"
+echo -e "${CYAN}***********************************************${DEFAULT}"
 
 # Cleanup again
 cd ../
@@ -63,4 +70,4 @@ rm -rf $Anykernel_DIR/dtbo.img
 # Build complete
 BUILD_END=$(date +"%s")
 DIFF=$(($BUILD_END - $BUILD_START))
-echo -e "$green Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$default"
+echo -e "${GREEN}Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.${DEFAULT}"
